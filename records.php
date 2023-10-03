61143090e7e421fdcf7f06aa2c13237378ad77ae
Rakesh Saini <143248825+krishmna@users.noreply.github.com>
2023-10-03 17:24:43 +0530

    added records.php

---
+++ records.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IF-edge" meta name="viewport"
+    content="width=device-width, initial-scale=1.0">
+  <title>Rakesh Saini</title>
+  <link rel="stylesheet" href="style.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;1,400&family=Roboto:ital,wght@0,300;1,300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <header class="header">
+    <a href="#" class="logo">Rk saini</a>
+
+    <nav class="navbar">
+      <a href="#Home" style="--i:1" class="active">Home</a>
+      <a href="#about" style="--i:2">About</a>
+      <a href="#skills" style="--i:3">Skills</a>
+      <a href="#services" style="--i:4">Services</a>
+      <a href="#Contact" style="--i:5">Contact</a>
+    </nav>
+  </header>
+  <section class="Home" id="Home">
+    <div class="hame-content">
+      <h3>Hello, It's Me </h3>
+      <h1>Rakesh saini</h1>
+      <h3>And I'm a <span id="element"></span></h3>
+      <P>I'm Graphic Designer With extensive experience for over 4 month
+        <br>expertise is to create and template design, logo design , and many more....
+      </P>
+      <div class="home-scl">
+        <a href="https://www.facebook.com/rakesh.tak.35762" style="--i:7"><i class='bx bxl-facebook'></i></a>
+        <a href="https://www.instagram.com/?next=%2F" style="--i:8"><i class='bx bxl-instagram'></i></a>
+        <a href="https://web.whatsapp.com" style="--i:9"><i class='bx bxl-whatsapp'></i></a>
+        <a href="https://www.youtube.com/@Rakesh35762" style="--i:10"><i class='bx bxl-youtube'></i></a>
+
+      </div>
+      <a href="https://mail.google.com/mail/u/1/#inbox" class="btn-box"> Home About Me</a>
+    </div>
+    </div>
+    </div>
+    <span class="home-imghover"></span>
+
+  </section>
+  <section class="about" id="about">
+    <div class="about-img">
+      <img src="my image.png" alt="">
+    </div>
+    <div class="about-text">
+      <h2>About <span>Me</span></h2>
+      <h4>Graphic Designer</h4>
+      <p>I'm a graphic designer with 1 year experience and have worked my way up to the creative director and main
+        graphic designer position at my company. I have extensive experience in print design, but my passion lies in web
+        design. I have worked with many
+        different types of clients.I'm a creative person driven by my passion for design. I'm proficient with graphic
+        tools and have an artistic approach towards everything I create.
+      </p>
+      <a href="https://www.instagram.com/?next=%2F" class="btn-box">More About Me</a>
+    </div>
+
+  </section>
+
+  <section>
+    <div class="services" id="services">
+      <div class="container">
+        <h1 class="sub-title"> My <span>Services</span></h1>
+        <div class="services-list">
+          <div>
+            <i class='bx bxl-microsoft' style='color:#00eeff'></i>
+            <h2>Software</h2>
+            <p>Software is a set of computer programs and associated documentation and data. This is in contrast to
+              hardware, from which the system is built and which actually performs the work.</p>
+            <a href="https://en.wikipedia.org/wiki/Software" class="read">learn more</a>
+          </div>
+          <div>
+            <i class='bx bx-edit-alt' style='color:#00eeff'  ></i>
+            <h2>Image Editing</h2>
+            <p>Image editing encompasses the processes of altering images, whether they are digital photographs,
+              traditional photo-chemical photographs, or illustrations.</p>
+            <a href="https://en.wikipedia.org/wiki/Image_editing" class="read">learn more</a>
+          </div>
+          <div>
+            <i class='bx bxs-camera' style='color:#00eeff'></i>
+            <h2>Photography</h2>
+            <p>Photography is the art, application, and practice of creating images by recording light, either
+              electronically by means of an image sensor, or chemically by means of a light-sensitive material such as
+              photographic film.</p>
+            <a href="https://en.wikipedia.org/wiki/Photography" class="read">learn more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <h1 class="sub-title">My <span>Skills</span></h1>
+   <section>
+    <div id="Rk Saini" id="project">
+      <div class="main-text" id="project">
+        <h2>Latest <span>project</span></h2>
+        <div class="Rk-contact">
+          <div class="row">
+            <img src="new design.png" alt="">
+            <div class="layer">
+              <h5> Communication</h5>
+              <p>Communication is usually defined as the transmission of information. The term can also refer to the
+                message itself, or the field of inquiry studying these transmissions, also known as communication
+                studies. The precise definition of communication is disputed.</p>
+
+              <a href="https://blog.shillingtoneducation.com/communication-design/"><i class='bx bx-link-external'
+                  style="color: rgb(0, 26, 255);"></i></a>
+            </div>
+          </div>
+          <div class="row">
+            <img src="1 big'.png" alt="">
+            <div class="layer">
+              <h5> Typography</h5>
+              <p>Typography is the art and technique of arranging type to make written language legible, readable and
+                appealing when displayed. The arrangement of type involves selecting typefaces, point sizes, line
+                lengths, line-spacing, and letter-spacing, as well as adjusting the space between pairs of letters.</p>
+
+              <a
+                href="https://99designs.com/blog/tips/typography-design/#:~:text=What%20is%20typography%20design%3F,with%20typefaces%20that%20already%20exist."><i
+                  class='bx bx-link-external' style="color: rgb(0, 26, 255);"></i></a>
+            </div>
+          </div>
+          <div class="row">
+            <img src="design-tool.png" alt="">
+            <div class="layer">
+              <h5> Web design</h5>
+              <p>Web design encompasses many different skills and disciplines in the production and maintenance of
+                websites. The different areas of web design include web graphic design; user interface design;
+                authoring, including standardised code and proprietary software; user experience design; and search
+                engine optimization.</p>
+
+              <a
+                href="https://www.betterteam.com/web-graphic-designer-job-description#:~:text=Also%20known%20as%20web%20designers,direction%20for%20the%20website's%20image."><i
+                  class='bx bx-link-external' style="color: rgb(0, 26, 255);"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="Contact" id="Contact">
+    <div class="Contact-text">
+      <h2>Contact <span>Me</span></h2>
+      <h4>Let's work Together</h4>
+      <p>Graphic design is a craft where professionals create visual content to communicate messages. By applying visual
+        hierarchy and page layout techniques, designers use typography and pictures to meet users’ specific needs and
+        focus on the logic of displaying elements in interactive designs, to optimize the user experience.</p>
+      <div class="contact-list">
+        <i class='bx bx-send'></i>
+        <li>takr7127@gmail.com</li>
+        <i class='bx bx-phone'></i>
+        <li>7425055439</li> 
+      </div>
+      <div class="contact-icons">
+        <a href="https://www.facebook.com/rakesh.tak.35762"><i class='bx bxl-facebook-circle'></i></a>
+        <a href="https://www.youtube.com/@Rakesh35762"><i class='bx bxl-youtube'></i></a>
+        <a href="https://www.linkedin.com/in/rakesh-saini-4116b2286/"><i class='bx bxl-linkedin'></i></a>
+        <a href="https://twitter.com/rakeshsaini2131"><i class='bx bxl-twitter'></i></a>
+      </div>
+
+    </div>
+
+    <div class="contact-form">
+      <form action="store.php" method="POST" >
+     
+        <input type="text" name="name" placeholder="Enter Your Name" autocomplete="off" required>
+
+        <input type="email" name="email"  id="email" placeholder="Enter Your Email" autocomplete="off" required>
+       
+        <input type="number" name="Number" placeholder="Enter your Number"  autocomplete="off" required>
+
+        <textarea  name="text" id="" cols="40"  rows="10" autocomplete="off" name="message"  placeholder="Enter Your Message" min="7" required></textarea>
+        <input type="submit" value="submit" name="sub" class="send" id="">
+        <?php   if(!empty($message)) { ?>
+        <div class="Success">
+          <strong><?php echo $message ?></strong>
+        </div>
+       <?php } ?>
+      </form>
+
+    </div>
+  </section>
+  <div class="last-text">
+    <p> Design With Rakesh Saini <a href="takr7127@gmail.com">Design With Rakesh Saini</p>
+
+  </div>
+  <a href="#" class="top"><i class='bx bx-up-arrow-alt'></i></a>
+
+
+
+
+  <script src="main.js"></script>
+
+</body>
+</html>          
