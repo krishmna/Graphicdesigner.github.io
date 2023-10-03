6c3e0d071586696c9e92309e6eda7b682d8f5a28
Rakesh Saini <143248825+krishmna@users.noreply.github.com>
2023-10-03 17:15:31 +0530

    added record.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BACK</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+   <div>
+     <h1>your information submit successfully</h1>
+    
+        <a href="records.php"> GO TO BACK</a>
+   </div> 
+ </body>
+</html>
---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#element', {
+  strings: ['<b><i>Rk saini</i></b>', 'Graphic designer', 'videoediter', '&amp; Writer' ],
+  typeSpeed: 70,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+ 
